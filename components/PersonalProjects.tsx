cb0ded2ed86ee80feb3241f4fd70c2acb8b1c53d
Laurie C <133490867+lmcrean@users.noreply.github.com>
2024-06-02 10:54:53 +0100

    Add steamreport.png as test for media hosting on deployment

--- components/PersonalProjects.tsx
+++ components/PersonalProjects.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Badge from 'react-bootstrap/Badge';
 import Stack from 'react-bootstrap/Stack';
-import Card from 'react-bootstrap/Card'; //todo: convert section to card
+import Card from 'react-bootstrap/Card'; //todo: convert section to card. https://react-bootstrap.netlify.app/docs/components/cards
 
 
 const PersonalProjects = () => {
@@ -11,7 +11,10 @@ const PersonalProjects = () => {
         <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4">
           {Array.from({ length: 5 }).map((_, index) => (
             <div key={index} className="bg-white shadow-md rounded-lg p-4">
-              <h3 className="text-lg font-semibold mb-2">Project {index + 1}</h3>
+              <div className='d-flex'>
+              <h3 className="text-lg font-semibold mb-2">Project {index + 1}</h3><a className='mr-0'>More info</a>
+              </div>
+              <img src="/images/steamreport.png" alt="SteamReport" />
               <p>A brief description of what Project {index + 1} does.</p>
               <br></br>
 
