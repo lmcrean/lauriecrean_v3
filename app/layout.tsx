f238984b2eea5fc453ca217896a861609840aa64
Laurie C <133490867+lmcrean@users.noreply.github.com>
2024-06-22 20:49:47 +0100

    refactor: Add favicon.ico and update layout.tsx to include favicon link

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="256x256" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
