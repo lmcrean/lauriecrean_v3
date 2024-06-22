79336bb437e56a031d0efb43bc69d94abe509ef0
Laurie C <133490867+lmcrean@users.noreply.github.com>
2024-06-22 20:58:02 +0100

    refactor: use Head import from next/head, attempt to fix favicon issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="256x256" />
-      </head>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
