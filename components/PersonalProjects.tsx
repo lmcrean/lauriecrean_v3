74b3441564aa7e8a248c46def4fae94a108567d1
Laurie C <133490867+lmcrean@users.noreply.github.com>
2024-07-04 13:50:31 +0100

    Update PersonalProjects component with overlay for selected project

--- components/PersonalProjects.tsx
+++ components/PersonalProjects.tsx
@@ -121,6 +121,25 @@ const PersonalProjects = () => {
       </div>
 
 <AnimatePresence>
+
+  {selectedId!== null && (
+    <motion.div
+      key="overlay"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 55, 0.8)', // Transparent blue overlay. Issue: expecting it to be hidden until a project is selected
+        zIndex: 999, // Ensure it's below the modal
+      }}
+    />
+  )}
   {selectedId!== null && (
     <motion.div
       key={selectedId}
@@ -141,6 +160,7 @@ const PersonalProjects = () => {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+        zIndex: 9999,
       }}
     >
       <motion.div
