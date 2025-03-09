53044442cef178f5debaa0884a4a44f5ff38010f
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-03-09 18:46:42 +0000

    chore: add Font Awesome CSS to Docusaurus configuration
    - Include Font Awesome CSS from CDN with integrity and crossorigin attributes
    - Enhance project styling and icon support in Docusaurus configuration

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -40,6 +40,14 @@ const config: Config = {
       href: 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css',
       type: 'text/css',
     },
+    // Add Font Awesome
+    {
+      href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+      type: 'text/css',
+      integrity: 'sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==',
+      crossorigin: 'anonymous',
+      referrerpolicy: 'no-referrer',
+    },
   ],
 
   // Even if you don't use internationalization, you can use this field to set
